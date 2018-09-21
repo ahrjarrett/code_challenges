19a3d623b4158b60e0ec47d9d5a006b9d5787535
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-20 20:14:56 -0500

    complete dependency injection :anger:

--- codewars/javascript/dependency_injection.js
+++ codewars/javascript/dependency_injection.js
@@ -3,28 +3,23 @@
 // Date   : 2018-08-29
 
 var deps = {
-  'dep1': function () {return 'this is dep1';},
-  'dep2': function () {return 'this is dep2';},
-  'dep3': function () {return 'this is dep3';},
-  'dep4': function () {return 'this is dep4';}
+  'dep1': function () {return 'this is dep1'},
+  'dep2': function () {return 'this is dep2'},
+  'dep3': function () {return 'this is dep3'},
+  'dep4': function () {return 'this is dep4'}
 }
 
 /**
  * Constructor DependencyInjector
- * @param {Object} - Object with keys of dependency names 
- *                   and the functions they map to as values
+ * @param {Object} - object with dependencies
  */
-var DI = function (dependencies) {
-  this.dependencies = dependencies
+var DI = function (dependency) {
+  this.dependency = dependency
 }
 
-/**  TODO: Extend this regex to work with arrow functions?    **
- **        How could I check which type of fn is passed?      **
- **        Also note that this regex does not take into       **
- **        account certain ES6 properties like destructuring  **/
-
 DI.prototype.inject = function(fn) {
-  let { dependencies } = this
+  if(!fn.length) return () => 0 // fn has no args? return 0
+  let { dependency: dependencies } = this
   let getArgsRegex = /function\s?\(\s?(.+)\s?\)\s?{/
   let getArgs = fn => fn.toString().match(getArgsRegex)[1]
   let fns = getArgs(fn)
@@ -33,14 +28,16 @@ DI.prototype.inject = function(fn) {
 
   return () => fn.apply(fn, fns)
 }
-  
+
+
 var di = new DI(deps)
+var di2 = new DI(deps)
 
 var myFunc = di.inject(function(dep3, dep1, dep2) {
   return [dep1(), dep2(), dep3()].join(' -> ')
 })
+var myOtherFunc = di2.inject(function() { return })
 
-console.log(
-  myFunc() // 'this is dep1 -> this is dep2 -> this is dep3'
-)
 
+console.log(myFunc())      // => this is dep1 -> this is dep2 -> this is dep3
+console.log(myOtherFunc()) // => 0

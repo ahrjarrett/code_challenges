0f0fa0ef657433365f238e6e8d4825b5c71a7009
Andrew Jarrett <ahrjarrett@gmail.com>
2018-10-03 00:09:13 -0500

    metaprogramming: builder of things 12 tests green

---
+++ codewars/javascript/metaprogramming/builder_of_things/builder_of_things.js
@@ -0,0 +1,142 @@
+// Source : https://www.codewars.com/kata/5571d9fc11526780a000011a/train/javascript
+// Author : Andrew Jarrett
+// Date   : 2018-10-02
+
+/*
+  
+ * For this kata you will be using some meta-programming magic to create a new 
+ * Thing object. This object will allow you to define things in a descriptive 
+ * sentence like format.
+
+ * This challenge attempts to build on itself in an increasingly complex manner.
+
+ * Examples of what can be done with "Thing":
+ 
+      const jane = new Thing('Jane')
+      jane.name // => 'Jane'
+      
+      // can define boolean methods on an instance
+      jane.is_a.person
+      jane.is_a.woman
+      jane.is_not_a.man
+      
+      jane.is_a_person // => true
+      jane.is_a_man // => false
+      
+      // can define properties on a per instance level
+      jane.is_the.parent_of.joe
+      jane.parent_of // => 'joe'
+      
+      // can define number of child things
+      // when more than 1, an array is created
+      jane.has(2).legs
+      jane.legs.length // => 2
+      jane.legs[0] instanceof Thing // => true
+      
+      // can define single items
+      jane.has(1).head
+      
+      jane.head instanceof Thing // => true
+      
+      // can define number of things in a chainable and natural format
+      jane.has(2).arms.each(arm => having(1).hand.having(5).fingers )
+      
+      jane.arms[0].hand.fingers.length // => 5
+      
+      // can define properties on nested items
+      jane.has(1).head.having(2).eyes.each( eye => being_the.color.blue.with(1).pupil.being_the.color.black )
+      
+      // can define methods
+      jane.can.speak('spoke', phrase =>
+        `${name} says: ${phrase}`)
+      
+      jane.speak("hello") // => "Jane says: hello"
+      
+      // if past tense was provided then method calls are tracked
+      jane.spoke // => ["Jane says: hello"]
+
+ */
+
+let STATE = {
+  NONE: 'NONE',
+  SET_BOOL: 'SET_BOOL',
+  HAS_PROP: 'HAS_PROP',
+}
+
+let singularize = plural => plural.replace(/s$/, '')
+
+class Thing {
+  constructor(name) {
+    this.name = name
+    this.__ = {
+      state: STATE.NONE,
+      value: null,
+      prev_prop: null
+    }
+    return make_proxy(this)
+  }
+}
+
+let make_proxy = instance => {
+  return new Proxy(instance, {
+
+    get: (target, prop, receiver) =>  {
+      let { state, value, prev_prop } = target.__
+
+      // console.log('\n')
+      // console.log('ARGS :', target.__)
+      // console.log('NEW PROP :', prop)
+
+      if(state === STATE.SET_BOOL) {
+	target[`${prev_prop}_${prop}`] = value
+	reset_proxy(target, prop)
+      }
+
+      if(state === STATE.HAS_PROP) {
+
+	target[prop] = value === 1
+	  ? new Thing(singularize(prop))
+	  : new Array(value).fill(new Thing(singularize(prop)))
+
+	reset_proxy(target, prop)
+      }
+
+      if(prop === 'has' || prop === 'having') {
+	return number => {
+	  target.__ = {
+	    state: STATE.HAS_PROP,
+	    value: number,
+	    prev_prop: 'has'
+	  }
+	  return receiver
+	}
+      }
+
+      if(prop === 'is_a' || prop === 'is_not_a') {
+	target.__ = {
+	  state: STATE.SET_BOOL,
+	  value: prop === 'is_a' ? true : false,
+	  prev_prop: 'is_a',
+	}
+	// calls fn again, passes in chain as new prop
+	return receiver
+      }
+
+      // default case: return prop
+      return target[prop]
+    }
+  })
+}
+
+let reset_proxy = (target, prev_prop) => {
+  target.__ = {
+    state: STATE.NONE,
+    value: null,
+    prev_prop,
+  }
+}
+
+module.exports = Thing
+
+
+
---
+++ codewars/javascript/metaprogramming/builder_of_things/test/builder_of_things.test.js
@@ -0,0 +1,162 @@
+const expect = require('chai').expect;
+const Thing = require('../builder_of_things.js')
+
+console.log('\n\n\nRUNNING TESTS\n')
+
+
+describe('Thing', () => {
+  describe('constructor', () => {
+    const jane = new Thing('Jane');
+    it('jane = new Thing("Jane") creates a new object', () => {
+      expect(jane).to.be.ok;
+    });
+    it('jane.name = "Jane"', () => {
+      expect(jane.name).to.equal('Jane');
+    });
+  });
+  describe('#is_a', () => {
+    describe('jane.is_a.woman', () => {
+      it('jane.is_a_woman should return true', () => {
+        const jane = new Thing('Jane');
+        jane.is_a.woman;
+        expect(jane.is_a_woman).to.equal(true);
+      });
+    });
+  });
+  describe('#is_not_a', () => {
+    describe('jane.is_not_a.man', () => {
+      it('jane.is_a_man should return false', () => {
+        const jane = new Thing('Jane');
+        jane.is_not_a.man;
+        expect(jane.is_a_man).to.equal(false);
+      });
+    });
+  });
+
+  describe('#has', () => {
+    describe('jane.has(2).arms', () => {
+      const jane = new Thing('Jane');
+      jane.has(2).arms;
+      it('jane.arms should be an array', () => {
+        expect(Array.isArray(jane.arms)).to.equal(true);
+      });
+
+      it('jane.arms should contain two Things', () => {
+        expect(jane.arms.length).to.equal(2);
+        expect(jane.arms.every(arm => arm instanceof Thing)).to.equal(true);
+      });
+
+      it('should call each Thing by its singular name ("arm")', () => {
+        expect(jane.arms[0].name).to.equal('arm');
+      });
+    });
+
+    describe('jane.having(2).arms', () => {
+      it('works like #has', () => {
+        const jane = new Thing('Jane');
+        jane.having(2).arms;
+        expect(jane.arms.length).to.equal(2);
+        expect(jane.arms[0].name).to.equal('arm');
+      });
+    });
+
+    describe('jane.has(1).head', () => {
+      const jane = new Thing('Jane');
+      jane.has(1).head;
+
+      it('creates a single Thing (not an array) as a property', () => {
+        expect(jane.head instanceof Thing).to.equal(true);
+      });
+
+      it('jane.head.name should be "head"', () => {
+        expect(jane.head.name).to.equal("head");
+      });
+    });
+
+    describe('jane.has(1).head.having(2).eyes', () => {
+      const jane = new Thing('Jane');
+      jane.has(1).head.having(2).eyes;
+
+      it('should create 2 new things on the head', () => {
+        expect(jane.head.eyes.length).to.equal(2);
+        expect(jane.head.eyes.every(eye => eye instanceof Thing)).to.equal(true);
+      });
+
+      it('should name the eye Thing "eye"', () => {
+        expect(jane.head.eyes[0].name).to.equal('eye');
+      });
+    });
+
+  });
+
+//   describe('#each', () => {
+//     describe('jane.has(2).hands.each(hand => having(5).fingers)', () => {
+//       const jane = new Thing('Jane');
+//       jane.has(2).hands.each(hand => having(5).fingers);
+
+//       it('should create 2 hands, each having 5 fingers', () => {
+//         expect(jane.hands.length).to.equal(2);
+//         expect(jane.hands[0].fingers.length).to.equal(5);
+//         expect(jane.hands[1].fingers.length).to.equal(5);
+//         expect(jane.hands[1].fingers[0].name).to.equal('finger');
+//       });
+//     });
+//   });
+
+//   describe('#is_the', () => {
+//     describe('jane.is_the.parent_of.joe', () => {
+//       const jane = new Thing('Jane');
+//       jane.is_the.parent_of.joe;
+
+//       it('jane.parent_of === "joe"', () => {
+//         expect(jane.parent_of).to.equal("joe");
+//       });
+//     });
+//   });
+
+//   describe('#being_the', () => {
+//     describe('jane.has(1).head.having(2).eyes.each(eye => being_the.color.green)', () => {
+//       const jane = new Thing('Jane');
+//       jane.has(1).head.having(2).eyes.each(eye => being_the.color.green);
+
+//       it('jane\'s eyes should both be green', () => {
+//         expect(jane.head.eyes[0].color).to.equal('green');
+//         expect(jane.head.eyes[1].color).to.equal('green');
+//       });
+//     });
+//   });
+
+//   describe('#and_the', () => {
+//     describe('jane.has(2).eyes.each(eye => being_the.color.blue.and_the.shape.round)', () => {
+//       const jane = new Thing('Jane');
+//       jane.has(1).head.having(2).eyes.each(eye => being_the.color.blue.and_the.shape.round);
+
+//       it('jane\'s eyes should both be blue and round', () => {
+//         expect(jane.head.eyes[0].color).to.equal('blue');
+//         expect(jane.head.eyes[0].shape).to.equal('round');
+//       });
+//     });
+//   });
+
+//   describe('#can', () => {
+//     describe('jane.can.speak(phrase => `${name} says: ${phrase}!`)', () => {
+//       const jane = new Thing('Jane');
+//       jane.can.speak(phrase => `${name} says: ${phrase}!`);
+
+//       it('should create a speak method on jane', () => {
+//         expect(jane.speak('hello')).to.equal('Jane says: hello!');
+//       });
+//     });
+
+//     describe('jane.can.speak("spoke", phrase => `${name} says: ${phrase}!`)', () => {
+//       const jane = new Thing('Jane');
+//       jane.can.speak('spoke', phrase => `${name} says: ${phrase}!`);
+
+//       it('jane.spoke should track the results of all calls to jane.speak(...)', () => {
+//         jane.speak('hi');
+//         expect(jane.spoke).to.deep.equal(['Jane says: hi!']);
+//       });
+//     });
+//   });
+
+});
